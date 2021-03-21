b3fc280f4486a3914bc6ea8d6ba744017fdbd8cf
sametdoqann <sametdoqann-s.doqann1570@gmail.com>
2021-03-21 13:20:31 +0300

    21 Mart Commit 2

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -39,12 +39,17 @@ export class ProductAddComponent implements OnInit {
       let productModel = Object.assign({}, this.productAddForm.value);
       this.productService.add(productModel).subscribe(
         (response) => {
-          console.log(response);
           this.toastrService.success(response.message, 'Başarılı');
         },
         (responseError) => {
-          console.log(responseError.error);
-          this.toastrService.error(responseError.error);
+          if (responseError.error.Errors.length > 0) {
+            for (let i = 0; i < responseError.error.Errors.length; i++) {
+              this.toastrService.error(
+                responseError.error.Errors[i].ErrorMessage,
+                'Doğrulama hatası'
+              );
+            }
+          }
         }
       );
     } else {
